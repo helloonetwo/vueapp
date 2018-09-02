6940c256b78b00f1b82afa272441b8cd9fd70e58
zxw <1774761877@qq.com>
2018-09-02 13:06:45 +0800

    新增加4个底部的组件和他们的路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,32 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Home from '../pages/Home/Home.vue'
+import Order from '../pages/Order/Order.vue'
+import Person from '../pages/Person/Person.vue'
+import Search from '../pages/Search/Search.vue' 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      redirect:  '/home'   //重定向到首页
+    },
+    {
+      path: '/home',
+      component: Home,     //首页路由
+    },
+    {
+      path: '/order',
+      component: Order,     //订单路由
+    },
+    {
+      path: '/search',
+      component: Search,    //搜索路由
+    },
+    {
+      path: '/person',
+      component: Person,   //个人路由
+    },
   ]
 })
